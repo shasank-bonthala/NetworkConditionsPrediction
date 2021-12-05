b77addafd9f88d89e0a4daf7262c7157fa3d58f8
shasank-bonthala <59098854+shasank-bonthala@users.noreply.github.com>
2021-12-04 18:28:55 -0800

    Update features.py

--- src/features.py
+++ src/features.py
@@ -1,1 +1,81 @@
+import pandas as pd
+import numpy as np
 
+# CUSTOM FEATURES
+
+# Feature 1: Maximum Packet Size for each interaction
+def max_size(x):
+  """
+  Helper function used to add max packet size feature
+  """
+    nums = x.split(';')[:-1]
+    nums_int = list(map(int, nums))
+    return max(numsInt)
+  
+# Feature 2: Range of Packet Size for each interaction
+def range_size(x):
+  """
+  Helper function used to add range of packet size feature
+  """
+    nums = x.split(';')[:-1]
+    nums_int = list(map(int, nums))
+    return max(numsInt) - min(numsInt)
+  
+# Feature 3: Average of Packet Size for each interaction
+def avg_size(x):
+  """
+  Helper function used to add average packet size feature
+  """
+    nums = x.split(';')[:-1]
+    numsInt = list(map(int, nums))
+    return np.mean(numsInt)
+  
+# Feature 4: Longest Packet Duration
+def packet_dur(x):
+  """
+  Helper function used to add longest packet duration feature
+  """
+    nums = x.split(';')[:-1]
+    numsInt = list(map(int, nums))
+    return max(numsInt) - min(numsInt)
+  
+# Feature 5: Total packet Direction
+def total_packet_dir(x):
+  """
+  Helper function used to add total packet direction feature
+  """
+    dirs = x.split(';')[:-1]
+    totalDirs = 0
+    
+    for i in dirs:
+        if i == '1':
+            totalDirs += 1
+        elif i == '2':
+            totalDirs -= 1
+           
+    return totalDirs
+  
+# Feature 6: total packets -> Done in apply_features()
+# Feature 7: total bytes -> Done in apply_features()
+
+# Feature 8: Interaction length
+def interaction_length(x):
+  """
+  Helper function used to add interaction length feature
+  """
+    times = x.split(';')[:-1]
+    times2 = list(map(int, times))
+    startTime = min(times2)
+    endTime = max(times2)
+           
+    return endTime - startTime
+  
+# Feature 9: total packets over time ratio -> Done in apply_features()
+# Feature 10: total bytes over time ratio -> Done in apply_features()
+
+def apply_features(df):
+  """
+  Takes in a raw dataframe from etl.py and 
+  applys all the custom features into one dataframe
+  """
+  
