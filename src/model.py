dfdad7de827a5e29fca63ce228961b6f47299717
shasank-bonthala <59098854+shasank-bonthala@users.noreply.github.com>
2021-12-05 17:12:35 -0800

    Update model.py

--- src/model.py
+++ src/model.py
@@ -9,19 +9,19 @@ def models():
         "BayesianRidge": linear_model.BayesianRidge
     }
 
-    return mdls
+  return mdls
 
 def model_build(features,truth,tag,test_size,**params):
   
-  X_train, X_test, y_train, y_test = train_test_split(features, truth, test_size=test_size)
+ X_train, X_test, y_train, y_test = train_test_split(features, truth, test_size=test_size)
   
-  if tag == 'packet_loss_ratio':
-    mdl = neighbors.KNeighborsRegressor(**params) 
-  elif tag == 'latency':
-    mdl = linear_model.BayesianRidge(**params)
+ if tag == 'packet_loss_ratio':
+   mdl = neighbors.KNeighborsRegressor(**params) 
+ elif tag == 'latency':
+   mdl = linear_model.BayesianRidge(**params)
     
-  mdl.fit(X_train, y_train)
-  preds = mdl.predict(X_test)
-  out = pd.concat([pd.Series(preds), y_test], axis=1)
+ mdl.fit(X_train, y_train)
+ preds = mdl.predict(X_test)
+ out = pd.concat([pd.Series(preds), y_test], axis=1)
   
  return out #returns a pandas df
