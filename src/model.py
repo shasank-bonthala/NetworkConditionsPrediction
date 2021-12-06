8dc0e2d4d3d074a61519cf987bb0fb28ac5c4615
shasank-bonthala <59098854+shasank-bonthala@users.noreply.github.com>
2021-12-05 16:23:14 -0800

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,1 +1,25 @@
+import pandas as pd
+from sklearn import linear_model
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+
+def models():
+  mdls = {
+        "SVM": svm.SVR,
+        "BayesianRidge": linear_model.BayesianRidge
+    }
+
+    return mdls
+
+def model_build(features,truth,tag,test_size,**params):
+  
+  X_train, X_test, y_train, y_test = train_test_split(features, truth, test_size=test_size)
+  
+  
+  
+  
+  
+  
+  
+  
 
