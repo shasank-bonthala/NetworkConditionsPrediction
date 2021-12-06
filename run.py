d93ad4476693f4c389d10401ef76aaa999e2630e
shasank-bonthala <59098854+shasank-bonthala@users.noreply.github.com>
2021-12-05 17:28:57 -0800

    Update run.py

--- run.py
+++ run.py
@@ -50,8 +50,8 @@ def main():
   #Latency Predictions
   latency_out = model_build(X, y1, 'latency', 0.33, **data_cfg['ridge_regressor'])
   
-  packet_loss_out.to_csv('data/out')
-  latency_out.to_csv('data/out')
+  packet_loss_out.to_csv('data/out/')
+  latency_out.to_csv('data/out/')
   
   return
   
